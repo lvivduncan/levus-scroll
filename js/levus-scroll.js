4e6f45a48ae2b31e60ee54a264a7cdcf769ac361
lvivduncan <lvivduncan@gmail.com>
2020-05-20 12:28:35 +0300

    add if exists

--- js/levus-scroll.js
+++ js/levus-scroll.js
@@ -2,102 +2,103 @@
   // scroll
   const scroll = document.querySelector('.levus-scroll');
 
-  // scroll elements
-  let items = scroll.querySelectorAll('.scroll-item');
-
-  // дублюємо всі елементи скрола
-  items.forEach(item => {
-    const clone = item.cloneNode(!0);
-    scroll.append(clone);
-  });
-
-  // new (all) scroll elements
-  items = scroll.querySelectorAll('.scroll-item');
-
-  // one item size
-  let width = 1;
-
-  // resize
-  window.addEventListener('resize', move);
-
-  // sizes
-  const sizes = [];
-
-  // z-index position
-  let zIdex = 0;
-
-  // fill sizes
-  items.forEach((item, i) => sizes.push(i * width - width));
-
-  // build scroll
-  move();
-
-  // left
-  const left = document.querySelector('.levus-scroll-nav .left');
-
-  // right
-  const right = document.querySelector('.levus-scroll-nav .right');
-
-  // scroll to left
-  left.addEventListener('click', () => {
-    const first = sizes.pop();
-    sizes.unshift(first);
+  // if exists
+  if(scroll != null){
+    // scroll elements
+    let items = scroll.querySelectorAll('.scroll-item');
+  
+    // дублюємо всі елементи скрола
+    items.forEach(item => {
+      const clone = item.cloneNode(!0);
+      scroll.append(clone);
+    });
+  
+    // new (all) scroll elements
+    items = scroll.querySelectorAll('.scroll-item');
+  
+    // one item size
+    let width = 1;
+  
+    // resize
+    window.addEventListener('resize', move);
+  
+    // sizes
+    const sizes = [];
+  
+    // z-index position
+    let zIdex = 0;
+  
+    // fill sizes
+    items.forEach((item, i) => sizes.push(i * width - width));
+  
+    // build scroll
     move();
-  });
-
-  // scroll to right
-  right.addEventListener('click', () => {
-    const last = sizes.shift();
-    sizes.push(last);
-    move();
-  });
-
-  // function move 1 item
-  function move() {
-
-    if (scroll.offsetWidth < 400) {
-      width = scroll.offsetWidth;
-      items.forEach((item, i) => {
-        item.style.width = `${width}px`;
-
-        item.style.transform = `translateX(${sizes[i] * width}px)`;
-
-        if (sizes[i] === 1) zIdex = 3;
-        else zIdex = 0;
-
-        item.style.zIndex = zIdex;
-      });
+  
+    // left
+    const left = document.querySelector('.levus-scroll-nav .left');
+  
+    // right
+    const right = document.querySelector('.levus-scroll-nav .right');
+  
+    // scroll to left
+    left.addEventListener('click', () => {
+      const first = sizes.pop();
+      sizes.unshift(first);
+      move();
+    });
+  
+    // scroll to right
+    right.addEventListener('click', () => {
+      const last = sizes.shift();
+      sizes.push(last);
+      move();
+    });
+  
+    // function move 1 item
+    function move() {
+  
+      if (scroll.offsetWidth < 400) {
+        width = scroll.offsetWidth;
+        items.forEach((item, i) => {
+          item.style.width = `${width}px`;
+  
+          item.style.transform = `translateX(${sizes[i] * width}px)`;
+  
+          if (sizes[i] === 1) zIdex = 3;
+          else zIdex = 0;
+  
+          item.style.zIndex = zIdex;
+        });
+      }
+  
+      else if (scroll.offsetWidth < 600) {
+        width = scroll.offsetWidth / 2;
+        items.forEach((item, i) => {
+          item.style.width = `${width}px`;
+  
+          item.style.transform = `translateX(${sizes[i] * width}px)`;
+  
+          if (sizes[i] === 0 || sizes[i] === 1 || sizes[i] === 2) zIdex = 3;
+          else zIdex = 0;
+  
+          item.style.zIndex = zIdex;
+        });
+      }
+  
+      else if (scroll.offsetWidth > 599) {
+        width = scroll.offsetWidth / 3;
+        items.forEach((item, i) => {
+          item.style.width = `${width}px`;
+  
+          item.style.transform = `translateX(${sizes[i] * width}px)`;
+  
+          if (sizes[i] === 1) zIdex = 3;
+          else if (sizes[i] ===  0 || sizes[i] === 2) zIdex = 2;
+          else zIdex = 0;
+  
+          item.style.zIndex = zIdex;
+        });
+      }
     }
-
-    else if (scroll.offsetWidth < 600) {
-      width = scroll.offsetWidth / 2;
-      items.forEach((item, i) => {
-        item.style.width = `${width}px`;
-
-        item.style.transform = `translateX(${sizes[i] * width}px)`;
-
-        if (sizes[i] === 0 || sizes[i] === 1 || sizes[i] === 2) zIdex = 3;
-        else zIdex = 0;
-
-        item.style.zIndex = zIdex;
-      });
-    }
-
-    else if (scroll.offsetWidth > 599) {
-      width = scroll.offsetWidth / 3;
-      items.forEach((item, i) => {
-        item.style.width = `${width}px`;
-
-        item.style.transform = `translateX(${sizes[i] * width}px)`;
-
-        if (sizes[i] === 1) zIdex = 3;
-        else if (sizes[i] ===  0 || sizes[i] === 2) zIdex = 2;
-        else zIdex = 0;
-
-        item.style.zIndex = zIdex;
-      });
-    }
-
-    console.log(sizes)
   }
 }
